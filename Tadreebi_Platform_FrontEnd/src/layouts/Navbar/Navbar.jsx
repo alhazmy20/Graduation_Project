1989140acc5119dd1daa64561f5212e08e0991b5
Abdrahman ALHRBI <68335728+mg-h@users.noreply.github.com>
2023-03-12 00:46:50 +0300

    nav with font

--- Tadreebi_Platform_FrontEnd/src/layouts/Navbar/Navbar.jsx
+++ Tadreebi_Platform_FrontEnd/src/layouts/Navbar/Navbar.jsx
@@ -1,9 +1,35 @@
 import React from "react";
 import "./Navbar.scss";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <nav className='navbar'>Navbar</nav>
+    <nav className="navbar">
+      <div className="container ">
+        <div className="logo">
+          <Link className="link" to="/">
+            تدريبي
+          </Link>
+        </div>
+        <div className="links">
+          <Link className="link" to="/?">
+            <h6>فرص التدريب</h6>
+          </Link>
+          <Link className="link" to="/?">
+            <h6>اخبار التدريب</h6>
+          </Link>
+          <Link className="link" to="/?">
+            <h6>نبذة عنا</h6>
+          </Link>
+          <Link className="link" to="/?">
+            <h6>تواصل معنا</h6>
+          </Link>
+        </div>
+      </div>
+      <div className="register">
+        <button>إنشاء حساب</button>
+        <button>تسجيل الدخول</button>
+      </div>
+    </nav>
   );
 };
 
