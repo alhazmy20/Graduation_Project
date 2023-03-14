54ecb8bf9764a1f80191123c80c9988a9cdcdc87
Abdrahman ALHRBI <68335728+mg-h@users.noreply.github.com>
2023-03-14 16:11:06 +0300

    New NavBar

--- Tadreebi_Platform_FrontEnd/src/layouts/Navbar/Navbar.jsx
+++ Tadreebi_Platform_FrontEnd/src/layouts/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import "../Navbar/Navbar.scss";
-import menu from "../../assets/images/menu.png";
-import { Drawer } from "antd";
+// import menu from "../../assets/images/menu.png";
+import { Drawer, Menu } from "antd";
+
 import { Link } from "react-router-dom";
+import { MenuOutlined } from "@ant-design/icons";
 
 const Navbar = () => {
- 
   const [open, setOpen] = useState(false);
 
   const showDrawer = () => {
@@ -17,57 +18,141 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="navbar">
-      <div className="container">
-        <img onClick={showDrawer} className="huborger" src={menu} alt=""></img>
-        <Drawer
-          rootClassName="mobile__menu"
-          placement="right"
-          onClose={onClose}
-          open={open}
-        >
-          <Link className="link" to="/">
-            <h6 className="mobile_link">تدريبي</h6>
-          </Link>
-          <Link className="link" to="/?">
-            <h6 className="mobile_link">فرص التدريب</h6>
-          </Link>
-          <Link className="link" to="/news">
-            <h6 className="mobile_link">اخبار التدريب</h6>
-          </Link>
-          <Link className="link" to="/?">
-            <h6 className="mobile_link">نبذة عنا</h6>
-          </Link>
-          <Link className="link" to="/?">
-            <h6 className="mobile_link">تواصل معنا</h6>
-          </Link>
-        </Drawer>
-        <div className="logo ">
-          <Link className="link" to="/">
-            تدريبي
-          </Link>
-        </div>
-        <div className="links ">
-          <Link className="link" to="/?">
-            <h6>فرص التدريب</h6>
-          </Link>
-          <Link className="link" to="/news">
-            <h6>اخبار التدريب</h6>
-          </Link>
-          <Link className="link" to="/#about" reloadDocument>
-            <h6>نبذة عنا</h6>
-          </Link>
-          <Link className="link" to="/?">
-            <h6>تواصل معنا</h6>
-          </Link>
-        </div>
+    // <nav className="navbar">
+    //   <div className="container">
+    //     <img onClick={showDrawer} className="huborger" src={menu} alt=""></img>
+    //     <Drawer
+    //       rootClassName="mobile__menu"
+    //       placement="right"
+    //       onClose={onClose}
+    //       open={open}
+    //     >
+    //       <Link className="link" to="/">
+    //         <h6 className="mobile_link">تدريبي</h6>
+    //       </Link>
+    //       <Link className="link" to="/?">
+    //         <h6 className="mobile_link">فرص التدريب</h6>
+    //       </Link>
+    //       <Link className="link" to="/news">
+    //         <h6 className="mobile_link">اخبار التدريب</h6>
+    //       </Link>
+    //       <Link className="link" to="/?">
+    //         <h6 className="mobile_link">نبذة عنا</h6>
+    //       </Link>
+    //       <Link className="link" to="/?">
+    //         <h6 className="mobile_link">تواصل معنا</h6>
+    //       </Link>
+    //     </Drawer>
+    //     <div className="logo ">
+    //       <Link className="link" to="/">
+    //         تدريبي
+    //       </Link>
+    //     </div>
+    //     <div className="links ">
+    // <Link className="link" to="/?">
+    //   <h6>فرص التدريب</h6>
+    // </Link>
+    //       <Link className="link" to="/news">
+    //         <h6>اخبار التدريب</h6>
+    //       </Link>
+    //       <Link className="link" to="/#about" reloadDocument>
+    //         <h6>نبذة عنا</h6>
+    //       </Link>
+    //       <Link className="link" to="/?">
+    //         <h6>تواصل معنا</h6>
+    //       </Link>
+    //     </div>
+    //   </div>
+    // <div className="register">
+    //   <button>إنشاء حساب</button>
+    //   <button>تسجيل الدخول</button>
+    // </div>
+    // </nav>
+    <div>
+      <div
+        className="menuIcon"
+        style={{
+          backgroundColor: "#249283",
+          height: 60,
+          paddingRight: 12,
+          paddingTop: 12,
+        }}
+      >
+        <MenuOutlined
+          style={{
+            color: "white",
+            fontSize: 30,
+          }}
+          onClick={() => {
+            setOpen(true);
+          }}
+        />
       </div>
+
+      <span className="headerMenu">
+        <AppMenu />
+      </span>
+
+      <Drawer
+        open={open}
+        onClose={() => setOpen(false)}
+        closable={false}
+        bodyStyle={{
+          backgroundColor: "#249283",
+          color: "white",
+          fontSize: 24,
+          textAlign: "center",
+          border: "none",
+        }}
+      >
+        <AppMenu isInline />
+      </Drawer>
+    </div>
+  );
+};
+
+const AppMenu = ({ isInline = false }) => {
+  return (
+    <div
+      className="appMenuContainer"
+     
+    >
+      <Menu
+        className="navBarMenu"
+        mode={isInline ? "inline" : "horizontal"}
+        items={[
+          {
+            label: "تدريبي",
+            key: "تدريبي",
+          },
+
+          {
+            label: "فرص التدريب",
+            key: "فرص التدريب",
+          },
+
+          {
+            label: "اخبار التدريب",
+            key: "اخبار التدريب",
+          },
+
+          {
+            label: "نبذة عنا",
+            key: "نبذة عنا",
+          },
+
+          {
+            label: "تواصل معنا",
+            key: "تواصل معنا",
+          },
+        ]}
+      ></Menu>
+
       <div className="register">
         <button>إنشاء حساب</button>
         <button>تسجيل الدخول</button>
       </div>
-    </nav>
+    </div>
   );
 };
-
 export default Navbar;
