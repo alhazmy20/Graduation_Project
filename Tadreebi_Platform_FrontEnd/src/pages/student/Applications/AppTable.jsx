0d833dc219f6ac1b0169e3251838b5bdff8507a8
ibrahim-ab <ibrahimmhm200@gmail.com>
2023-03-15 20:26:21 +0300

    pagination fix and showing number of apps in text

--- Tadreebi_Platform_FrontEnd/src/pages/student/Applications/AppTable.jsx
+++ Tadreebi_Platform_FrontEnd/src/pages/student/Applications/AppTable.jsx
@@ -1,7 +1,10 @@
 import { Table } from "antd";
 import "./AppTable.scss";
+import { itemRender } from "../../../components/ui/Pagination"
+import { useState } from "react";
 
 const AppTable = () => {
+  
   const dataSource = [
     {
       key: "1",
@@ -24,7 +27,22 @@ const AppTable = () => {
       appname: "xxxxxx",
       appdate: "2023",
     },
+    {
+      key: "4",
+      appstat: "تم القبول",
+      instname: "xxxxx",
+      appname: "xxxxxx",
+      appdate: "2023",
+    },
+    {
+      key: "5",
+      appstat: "تم القبول",
+      instname: "xxxxx",
+      appname: "xxxxxx",
+      appdate: "2023",
+    },
   ];
+  
   const columns = [
     {
       title: "تاريخ الطلب",
@@ -47,10 +65,33 @@ const AppTable = () => {
       align: "center",
     },
   ];
+
+  const [pageSize, setPageSize] = useState(3);
+  const [currentRange, setCurrentRange] = useState([1, pageSize]);
+  
+  const handlePaginationChange = (page, pageSize) => {
+    const start = (page - 1) * pageSize + 1;
+    const end = Math.min(start + pageSize - 1, dataSource.length);
+    setCurrentRange([start, end]);
+    setPageSize(pageSize);
+  };
+  
   return (
     <div className="AppTable">
-      <p className="tex">عرض 1 إلى 3 من أصل 6 سجل</p>
-      <Table dataSource={dataSource} columns={columns} />
+      <p className="tex">
+        عرض {currentRange[0]} إلى {currentRange[1]} من أصل {dataSource.length} سجل
+      </p>
+      <Table
+        classname="tab"
+        dataSource={dataSource}
+        columns={columns} 
+        pagination={{
+          onChange: handlePaginationChange,
+          responsive: true,
+          itemRender: itemRender,
+          pageSize: pageSize,
+        }}
+      />
     </div>
   );
 };
