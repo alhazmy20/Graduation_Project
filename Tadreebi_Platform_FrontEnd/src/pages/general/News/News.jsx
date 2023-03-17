4a937d874c5ebd112c6bc472705deb2ed01a0f9e
Abdulmohsen Alnasser <60625460+Abdu1mo7sen@users.noreply.github.com>
2023-03-17 23:12:08 +0300

    Add fake API for testing
    add fake api using json-server to use it with news page.
    
    to turn the server on use the commands below:
    
    first type: npm install / to install dependencies.
    
    then
    
    type:
     npx json-server --watch .\src\pages\general\News\db.json --port 8000
    
    you can check if the API is turned on or not by typing the url with port 8000 like: localhost:8000/news
    then it will show you news json objects

--- Tadreebi_Platform_FrontEnd/src/pages/general/News/News.jsx
+++ Tadreebi_Platform_FrontEnd/src/pages/general/News/News.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Button, Card, Image, List} from "antd";
 import Title from "antd/es/typography/Title";
 import instPng from "../../../assets/images/image14.png";
+import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import "./News.scss"
@@ -13,18 +14,15 @@ const { Meta } = Card;
 const News = () => {
   const navigate = useNavigate();
 
-  const data = Array.from({
-    length: 25,
-  }).map((_, i) => ({
-    id: i,
-    title: `تعلن سدايا عن موعد بدء التقديم على التدريب التعاوني للترم
-       الثاني`,
-    avatar: instPng,
-    date: "منذ 3 أيام",
-    description: "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم، ولكن الغالبية تم تعديلها بشكل ما عبر إدخال بعض النوادر أو الكلمات العشوائية إلى النص. إن كنت تريد أن تستخدم نص لوريم إيبسوم ما، عليك أن تتحقق أولاً أن ليس هناك أي كلمات أو عبارات محرجة أو غير لائقة مخبأة في هذا النص. بينما تعمل جميع مولّدات نصوص لوريم إيبسوم على الإنترنت على إعادة تكرار مقاطع من نص لوريم إيبسوم نفسه عدة مرات بما تتطلبه الحاجة، يقوم مولّدنا هذا باستخدام كلمات من قاموس يحوي على أكثر من 200 كلمة لا تينية، مضاف إليها مجموعة من الجمل النموذجية، لتكوين نص لوريم إيبسوم ذو شكل منطقي قريب إلى النص الحقيقي. وبالتالي يكون النص الناتح خالي من التكرار، أو أي كلمات أو عبارات غير لائقة أو ما شابه. وهذا ما يجعله أول مولّد نص لوريم إيبسوم حقيقي على الإنترنت.",
-  }));
-
+  const [news,setNews] = useState([]);
   
+  const url = "http://localhost:8000/news"
+  useEffect(() => {
+    axios.get(url)
+    .then((response) => {
+      setNews(response.data);
+    })
+  }, [])
 
   return (
      <List
@@ -41,7 +39,7 @@ const News = () => {
         align: "center",
         pageSize: 7,
       }}
-      dataSource={data}
+      dataSource={news}
       renderItem={(news) => (
         <List.Item
         className="listItemContainer"
