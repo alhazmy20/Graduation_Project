bfac44ac37cc1d68455203538e28fc614e3a592b
Abdulmohsen Alnasser <60625460+Abdu1mo7sen@users.noreply.github.com>
2023-04-06 02:58:45 +0300

    sidebar

--- Tadreebi_Platform_FrontEnd/src/layouts/AdminSidebar/AdminSidebar.jsx
+++ Tadreebi_Platform_FrontEnd/src/layouts/AdminSidebar/AdminSidebar.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import './AdminSidebar.scss'
+import { Sidebar,SubMenu, Menu, MenuItem, useProSidebar } from 'react-pro-sidebar';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome,faPerson,faClipboard,faInfoCircle,faSignOut } from "@fortawesome/free-solid-svg-icons";
 
 const AdminSidebar = () => {
+  const { collapseSidebar, toggleSidebar, collapsed, toggled, broken, rtl } = useProSidebar();
   return (
-    <div className='admin-sidebar'>AdminSidebar</div>
+    <div className='SideBarContainer'>
+      <Sidebar className='Sidebar' rtl>
+        <div className='platformContainer'>
+          <span className='Platform'>تدريبي</span>
+          <span className='Name'>عبدالمحسن الحازمي</span>
+          <hr className='hLine'/>
+        </div>
+  <Menu>
+    <MenuItem className='MenuItem' icon={<div style={{backgroundColor:'#F6F6F6'}}>
+      <FontAwesomeIcon  icon={faHome} />
+    </div>}>الرئيسية</MenuItem>
+    <SubMenu className='MenuItem' label="ادارة المستخدمين" icon={<FontAwesomeIcon  icon={faPerson} />}>
+      <MenuItem className='SubMenu'> ادارة المؤسسات </MenuItem>
+      <MenuItem className='SubMenu'> ادارة الطلاب </MenuItem>
+      <MenuItem className='SubMenu'> ادارة المشرفين </MenuItem>
+    </SubMenu>
+    <SubMenu className='MenuItem' label="ادارة المحتوى" icon={<FontAwesomeIcon  icon={faClipboard} />}>
+    <MenuItem className='SubMenu'>ادارة فرص التدريب</MenuItem>
+    <MenuItem className='SubMenu'>ادارة اخبار التدريب</MenuItem>
+    </SubMenu>
+    <MenuItem className='MenuItem' icon={<FontAwesomeIcon  icon={faInfoCircle} />}> الملف الشخصي </MenuItem>
+    <MenuItem className='MenuItem' icon={<FontAwesomeIcon  icon={faSignOut} />}> تسجيل خروج </MenuItem>
+  </Menu>
+</Sidebar>
+    </div>
   )
 }
 
