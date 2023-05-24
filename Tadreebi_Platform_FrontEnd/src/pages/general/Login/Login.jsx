607ae4df8672e482f32ff2ebb41d13e3765b9b0c
Abdulmohsen Alnasser <60625460+Abdu1mo7sen@users.noreply.github.com>
2023-05-24 04:45:02 +0300

    d

--- Tadreebi_Platform_FrontEnd/src/pages/general/Login/Login.jsx
+++ Tadreebi_Platform_FrontEnd/src/pages/general/Login/Login.jsx
@@ -72,7 +72,6 @@ const Login = ({ isAdmin }) => {
               </span>
               <span className="goHome">
                 <Link to="/">الذهاب الى الصفحة الرئيسية</Link>
-                <Link to="/">asdasdasd</Link>
               </span>
             </Form>
           </FormCard>
