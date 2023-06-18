06349e9d438663471638a095b8e53e87c48e3ee3
Abdulmohsen Alnasser <60625460+Abdu1mo7sen@users.noreply.github.com>
2023-06-18 14:02:27 +0300

    .

--- Tadreebi_Platform_FrontEnd/src/layouts/Footer/Footer.jsx
+++ Tadreebi_Platform_FrontEnd/src/layouts/Footer/Footer.jsx
@@ -6,7 +6,7 @@ import twitter from "../../assets/images/twitter.png";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="copy">جميع الحقوق محفوظة لشركة تدريبي © 2023</div>
+      <div className="copy">2023</div>
       <div className="social">
         <img src={twitter} alt=""></img>
         <img src={instagram} alt=""></img>
